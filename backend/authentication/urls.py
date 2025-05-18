2521cfddd6b840aaa5950dcb2f9b5125b7f2821e
kelvinleandro <kelvinleandro5@gmail.com>
2025-05-17 21:00:14 -0300

    refactor(back): remove signup endpoint

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import SignupView, LoginView, LogoutView
+from .views import LoginView, LogoutView
 from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
-    path("signup/", SignupView.as_view(), name="auth-signup"),
     path("login/", LoginView.as_view(), name="auth-login"),
     path("refresh/", TokenRefreshView.as_view(), name="auth-refresh"),
     path("logout/", LogoutView.as_view(), name="auth-logout"),
